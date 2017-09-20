1ef9dccf0db59cb40a68f85d4f95418659feb5b8
Shashwat Pandey <shashwat.pandey@thomsonreuters.com>
2017-09-20 12:16:16 -0400

    Added semantic-ui and flask

---
+++ app.py
@@ -0,0 +1,9 @@
+from flask import Flask, render_template, request, redirect, url_for
+import config
+
+app = Flask(__name__, static_url_path='')
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
---
+++ config.py
@@ -0,0 +1,1 @@
+news_api = {'key': '30423ef4daa24d869d8ef76e2ed3d062'}
