7d0f3010193ffc8d2681f79d01afd2ef7ed46610
Shashwat Pandey <shashwat.pandey@thomsonreuters.com>
2017-09-20 12:43:57 -0400

    Added in requests for breitbart, cnn and fox

--- app.py
+++ app.py
@@ -8,3 +8,9 @@ app = Flask(__name__, static_url_path='')
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/indexData')
+def get_index_data():
+    breitbart = requests.get("http://beta.newsapi.org/v2/everything?source=breitbart-news&apiKey="+config.news_api['key'])
+    cnn = requests.get("http://beta.newsapi.org/v2/everything?source=cnn&apiKey="+config.news_api['key'])
+    fox = requests.get("http://beta.newsapi.org/v2/everything?source=fox-news&apiKey="+config.news_api['key'])
